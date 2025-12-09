ad5ab2df17d84f261f7ed44b7106d37635c84425
kruthikav04 <kruthika04v2gmail.com>
2025-12-09 12:18:45 +0000

    updated app file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from AIROWIRE Networ!"
+    return "Hello from AIROWIRE Networks!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
